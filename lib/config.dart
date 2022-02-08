5d4c5816f6ec907c5c9e2c09dfc4b1ccfbde1e5c
hafizhan.dev <hafizhan.dev@gmail.com>
2022-02-09 00:24:08 +0700

    first commit

---
+++ lib/config.dart
@@ -0,0 +1,15 @@
+/// Get your own App ID at https://dashboard.agora.io/
+const String appId = '2192e1a709624d8bbc66f45fec4e91da';
+
+/// Please refer to https://docs.agora.io/en/Agora%20Platform/token
+const String token =
+    '0062192e1a709624d8bbc66f45fec4e91daIABU9oWelxn8LgZGXz8CbIRP1JAT2S/ndewhxel4eg2RPQjZSCUAAAAAEADbqsx0mOoDYgEAAQCZ6gNi';
+
+/// Your channel ID
+const String channelId = '<YOUR_CHANNEL_ID>';
+
+/// Your int user ID
+const int uid = 0;
+
+/// Your string user ID
+const String stringUid = '0';
